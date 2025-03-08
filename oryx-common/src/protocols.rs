d3ad49503c3d63a9e65d98184e3776f1eedb745e
Badr <contact@pythops.com>
2025-03-08 12:42:31 +0100

    use strum crate

--- oryx-common/src/protocols.rs
+++ oryx-common/src/protocols.rs
@@ -1,4 +1,4 @@
-use core::{fmt::Display, str::FromStr};
+use strum::{AsRefStr, Display, EnumString};
 
 #[derive(Debug, Copy, Clone)]
 #[repr(C, u8)]
@@ -15,10 +15,13 @@ pub struct ParseProtocolError;
 
 pub const NB_TRANSPORT_PROTOCOL: u16 = 2;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, AsRefStr, Display, EnumString)]
 #[repr(C)]
 pub enum TransportProtocol {
+    #[strum(ascii_case_insensitive)]
     TCP = 0,
+
+    #[strum(ascii_case_insensitive)]
     UDP = 1,
 }
 
@@ -28,35 +31,20 @@ impl TransportProtocol {
     }
 }
 
-impl Display for TransportProtocol {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            TransportProtocol::TCP => write!(f, "Tcp"),
-            TransportProtocol::UDP => write!(f, "Udp"),
-        }
-    }
-}
-
-impl FromStr for TransportProtocol {
-    type Err = ParseProtocolError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Udp" | "udp" => Ok(Self::UDP),
-            "Tcp" | "tcp" => Ok(Self::TCP),
-            _ => Err(ParseProtocolError),
-        }
-    }
-}
-
 // Network Protocols
 
 pub const NB_NETWORK_PROTOCOL: u16 = 3;
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, AsRefStr, Display, EnumString)]
 #[repr(C)]
 pub enum NetworkProtocol {
+    #[strum(ascii_case_insensitive)]
     Ipv4 = 0,
+
+    #[strum(ascii_case_insensitive)]
     Ipv6 = 1,
+
+    #[strum(ascii_case_insensitive)]
     Icmp = 2,
 }
 
@@ -70,56 +58,19 @@ impl NetworkProtocol {
     }
 }
 
-impl Display for NetworkProtocol {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            NetworkProtocol::Ipv4 => write!(f, "Ipv4"),
-            NetworkProtocol::Ipv6 => write!(f, "Ipv6"),
-            NetworkProtocol::Icmp => write!(f, "Icmp"),
-        }
-    }
-}
-
-impl FromStr for NetworkProtocol {
-    type Err = ParseProtocolError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Ipv4" | "ipv4" => Ok(Self::Ipv4),
-            "Ipv6" | "ipv6" => Ok(Self::Ipv6),
-            "Icmp" | "icmp" => Ok(Self::Icmp),
-            _ => Err(ParseProtocolError),
-        }
-    }
-}
-
 // Link Protocols
 
 pub const NB_LINK_PROTOCOL: u16 = 1;
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, AsRefStr, Display, EnumString)]
 #[repr(C)]
 pub enum LinkProtocol {
+    #[strum(ascii_case_insensitive)]
     Arp = 0,
 }
 
 impl LinkProtocol {
     pub fn all() -> [LinkProtocol; 1] {
         [LinkProtocol::Arp]
-    }
-}
-
-impl Display for LinkProtocol {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "Arp")
-    }
-}
-
-impl FromStr for LinkProtocol {
-    type Err = ParseProtocolError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Arp" | "arp" => Ok(Self::Arp),
-            _ => Err(ParseProtocolError),
-        }
     }
 }
